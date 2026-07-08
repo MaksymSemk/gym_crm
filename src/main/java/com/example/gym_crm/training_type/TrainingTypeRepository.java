088a9aaa7db1114c610564236c8519ec64316e56
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-08 23:49:57 +0300

    Implemented repos for entities except training

--- src/main/java/com/example/gym_crm/training_type/TrainingTypeRepository.java
+++ src/main/java/com/example/gym_crm/training_type/TrainingTypeRepository.java
@@ -1,23 +1,8 @@
 package com.example.gym_crm.training_type;
 
-import com.example.gym_crm.common.id_generator.IdGenerator;
-import com.example.gym_crm.common.repository.AbstractRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
-
 @Repository
-public class TrainingTypeRepository extends AbstractRepository<String, TrainingType> {
-    @Autowired
-    @Override
-    public void setStorage(Map<String, TrainingType> trainingTypeStorage) {
-        super.setStorage(trainingTypeStorage);
-    }
-
-    @Autowired
-    @Override
-    public void setIdGenerator(IdGenerator<String> stringIdGenerator) {
-        super.setIdGenerator(stringIdGenerator);
-    }
+public interface TrainingTypeRepository extends JpaRepository<TrainingType, Long> {
 }
