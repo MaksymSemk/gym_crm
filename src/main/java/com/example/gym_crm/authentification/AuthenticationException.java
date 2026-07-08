a4ef813b1057977fcdd91eb189eda34dfb15ce08
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-09 00:06:40 +0300

    Added to security authData, authentication exception and implemented authenticate method

---
+++ src/main/java/com/example/gym_crm/authentification/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.example.gym_crm.authentification;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/gym_crm/authentification/SecurityAspect.java
+++ src/main/java/com/example/gym_crm/authentification/SecurityAspect.java
@@ -1,17 +1,41 @@
 package com.example.gym_crm.authentification;
 
+import com.example.gym_crm.common.user.UserRepository;
+import lombok.AllArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Aspect
 @Component
+@AllArgsConstructor
 public class SecurityAspect {
+
+    private UserRepository userRepository;
 
     @Before("@annotation(requiresAuth)")
     public void authenticate(JoinPoint joinPoint, RequiresAuth requiresAuth) {
+        AuthData authData = null;
 
+        for(Object o : joinPoint.getArgs()) {
+            if(o instanceof AuthData) {
+                authData = (AuthData) o;
+                break;
+            }
+        }
+
+        if (authData == null) {
+            throw new AuthenticationException("Authentication failed");
+        }
+
+        var user =  userRepository.findByUsername(authData.username()).orElseThrow(
+                ()-> new AuthenticationException("Authentication failed")
+        );
+        var password = authData.password();
+
+        if (!password.equals(user.getPassword())) {
+            throw new AuthenticationException("Authentication failed");
+        }
     }
 }
