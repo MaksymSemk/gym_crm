f859811c9ea8d7db26cde06e68d12b99b668724b
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-08 22:07:42 +0300

    Made training type as entity

--- src/main/java/com/example/gym_crm/training_type/TrainingType.java
+++ src/main/java/com/example/gym_crm/training_type/TrainingType.java
@@ -1,21 +1,30 @@
 package com.example.gym_crm.training_type;
 
 import com.example.gym_crm.common.repository.EntityId;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class TrainingType implements EntityId<String> {
+@Entity
+@Table(name = "training_types")
+public class TrainingType implements EntityId<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
 
     @Override
-    public String getId() {
-        return this.name;
+    public Long getId() {
+        return this.id;
     }
 
     @Override
-    public void setId(String s) {
-        this.name = s;
+    public void setId(Long id) {
+        this.id = id;
     }
 }
