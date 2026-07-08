d54659997f11e17b36bd61d75fba476ed15d8e8e
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-08 23:49:15 +0300

    src/main/java/com/example/gym_crm/training_type/TrainingTypeRepository.java

--- src/main/java/com/example/gym_crm/trainee/TraineeRepository.java
+++ src/main/java/com/example/gym_crm/trainee/TraineeRepository.java
@@ -3,23 +3,12 @@ package com.example.gym_crm.trainee;
 import com.example.gym_crm.common.id_generator.IdGenerator;
 import com.example.gym_crm.common.repository.AbstractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.UUID;
 
 @Repository
-public class TraineeRepository extends AbstractRepository<UUID, Trainee> {
-
-    @Autowired
-    @Override
-    public void setStorage(Map<UUID, Trainee> traineeStorage) {
-        super.setStorage(traineeStorage);
-    }
-
-    @Autowired
-    @Override
-    public void setIdGenerator(IdGenerator<UUID> uuidGenerator) {
-        super.setIdGenerator(uuidGenerator);
-    }
+public interface TraineeRepository extends JpaRepository<Trainee, UUID> {
 }
--- src/main/java/com/example/gym_crm/trainer/TrainerRepository.java
+++ src/main/java/com/example/gym_crm/trainer/TrainerRepository.java
@@ -4,22 +4,12 @@ import com.example.gym_crm.common.id_generator.IdGenerator;
 import com.example.gym_crm.common.repository.AbstractRepository;
 import com.example.gym_crm.trainee.Trainee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.UUID;
 
 @Repository
-public class TrainerRepository extends AbstractRepository<UUID, Trainer> {
-    @Autowired
-    @Override
-    public void setStorage(Map<UUID, Trainer> trainerStorage) {
-        super.setStorage(trainerStorage);
-    }
-
-    @Autowired
-    @Override
-    public void setIdGenerator(IdGenerator<UUID> uuidGenerator) {
-        super.setIdGenerator(uuidGenerator);
-    }
+public interface TrainerRepository extends JpaRepository<Trainer, UUID> {
 }
