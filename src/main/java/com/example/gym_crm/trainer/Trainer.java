5fef63e16fd68c10cd77dfa36729584441dc3df0
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-08 22:16:01 +0300

    Made trainer as entity

--- src/main/java/com/example/gym_crm/trainer/Trainer.java
+++ src/main/java/com/example/gym_crm/trainer/Trainer.java
@@ -1,38 +1,45 @@
 package com.example.gym_crm.trainer;
 
 import com.example.gym_crm.common.repository.EntityId;
-import com.example.gym_crm.training.Training;
-import com.example.gym_crm.training_type.TrainingType;
 import com.example.gym_crm.common.user.User;
+import com.example.gym_crm.training_type.TrainingType;
+import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
-public class Trainer extends User implements EntityId<UUID> {
+@Entity
+@Table(name = "trainers")
+public class Trainer implements EntityId<UUID> {
 
-    private Set<TrainingType> specialization;
-    private UUID userId;
-    private Set<Training> trainings;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
-    public Trainer(String firstName, String lastName, String username, String password, boolean isActive, Set<TrainingType> specialization) {
-        super(firstName, lastName, username, password, isActive);
-        this.specialization = specialization;
-        this.userId = UUID.randomUUID();
-        this.trainings = new HashSet<>();
-    }
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "trainer_training_type",
+            joinColumns = @JoinColumn(name = "trainer_id"),
+            inverseJoinColumns = @JoinColumn(name = "training_type_id")
+    )
+    private List<TrainingType> specialization;
+    //private Set<Training> trainings;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
     @Override
     public UUID getId() {
-        return userId;
+        return user.getId();
     }
 
     @Override
-    public void setId(UUID uuid) {
-        this.userId = uuid;
+    public void setId(UUID id) {
+        this.user.setId(id);
     }
 }
