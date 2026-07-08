82b63bc2b0a019601c9f0030d86dadb1cb4fa252
MaksymSemk <maksumsemen2006@gmail.com>
2026-07-08 23:39:01 +0300

    Created repository for user entities and updated user utils

--- src/main/java/com/example/gym_crm/common/user/UserUtils.java
+++ src/main/java/com/example/gym_crm/common/user/UserUtils.java
@@ -10,8 +10,7 @@ import java.security.SecureRandom;
 @Component
 public class UserUtils {
 
-    private TrainerRepository trainerRepository;
-    private TraineeRepository traineeRepository;
+    private UserRepository userRepository;
 
     private static final String CHARACTERS =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
@@ -19,14 +18,10 @@ public class UserUtils {
     private static final SecureRandom random = new SecureRandom();
 
     @Autowired
-    public void setTrainerRepository(TrainerRepository trainerRepository) {
-        this.trainerRepository = trainerRepository;
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    @Autowired
-    public void setTraineeRepository(TraineeRepository traineeRepository) {
-        this.traineeRepository = traineeRepository;
-    }
 
     public String createUsername(String firstName, String lastName) {
         if (firstName == null || lastName == null) {
@@ -37,10 +32,7 @@ public class UserUtils {
         String resultUsername = initialUsername;
         int counter = 1;
 
-        var trainers = trainerRepository.findAll().stream().map(User::getUsername).toList();
-        var trainee = traineeRepository.findAll().stream().map(User::getUsername).toList();
-
-        while (trainers.contains(resultUsername) || trainee.contains(resultUsername)) {
+        while (userRepository.findByUsername(resultUsername).isPresent()) {
             resultUsername = initialUsername + counter;
             counter++;
         }
