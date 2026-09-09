4d20a37d219c29bf629d3c28b4e30fc3982aa2d4
MaksymSemk <maksumsemen2006@gmail.com>
2026-09-09 22:00:59 +0300

    set up document for TrainerWorkload

--- trainer-workload-service/src/main/java/com/example/trainerworkloadservice/workload/model/TrainerWorkload.java
+++ trainer-workload-service/src/main/java/com/example/trainerworkloadservice/workload/model/TrainerWorkload.java
@@ -1,19 +1,41 @@
 package com.example.trainerworkloadservice.workload.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@CompoundIndex(
+        name = "trainer_name_idx",
+        def = "{'trainerFirstName': 1, 'trainerLastName': 1}"
+)
 public class TrainerWorkload {
+
+    @Id
+    @Field("trainerUsername")
     private String trainerUsername;
+
+    @Field("trainerFirstName")
     private String trainerFirstName;
+
+    @Field("trainerLastName")
     private String trainerLastName;
+
+    @Field("trainerStatus")
     private Boolean trainerStatus;
+
+    @Builder.Default
+    @Field("years")
     private List<YearSummary> years = new ArrayList<>();
 }
